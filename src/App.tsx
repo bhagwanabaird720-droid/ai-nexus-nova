bfb8976fc29f9c8ec93d7b1ba6ef8b7b3b9ad990
bhagwanabaird720-droid <bhagwanabaird720@gmail.com>
2026-05-26 20:23:09 +0530

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+export default function App() {
+  return (
+    <div style={{
+      minHeight: "100vh",
+      background: "#000",
+      color: "#fff",
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      fontSize: "32px"
+    }}>
+      AI Nexus Nova Working 🚀
+    </div>
+  );
+}
